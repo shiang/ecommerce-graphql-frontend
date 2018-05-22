a24d76b648af6c42cfa7af2f978fee68d75ea7f1
Ryan Liu <shiang@Ryans-MacBook-Pro.local>
2018-05-22 18:38:41 +1000

    Update header in log out state. Added logout button.

--- src/CustomerLayout.js
+++ src/CustomerLayout.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { Layout, Menu, Breadcrumb, Avatar, Icon, Spin, Popover } from "antd";
-import { Image } from 'semantic-ui-react'
+import { Image } from "semantic-ui-react";
 import { Search } from "./App";
 import axios from "axios";
 import { Query, Subscription } from "react-apollo";
 import { FETCH_CUSTOMER } from "./queries";
 import { SUBSCRIBE_CART } from "./subscriptions";
 import HeaderLogo from "./image/dot-shop-logo.png";
-import { Link } from 'react-router-dom'
-
+import { Link } from "react-router-dom";
 
 const { Header, Content, Footer } = Layout;
 
@@ -36,6 +35,10 @@ class CustomerLayout extends React.Component {
     });
   };
 
+  logOut = () => {
+    localStorage.removeItem("customerId");
+  }
+
   render() {
     const { customerId } = this.state;
     console.log(customerId);
@@ -49,10 +52,41 @@ class CustomerLayout extends React.Component {
                 position: "fixed",
                 width: "100%",
                 zIndex: 500,
-                textAlign: "right"
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between"
               }}
             >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  backgroundColor: "#fff",
+                  padding: "5px",
+                  margin: "5px"
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    backgroundColor: "#fff",
+                    padding: "5px",
+                    margin: "5px"
+                  }}
+                >
+                  <Link to="/">
+                    <Image
+                      src={HeaderLogo}
+                      alt="logo"
+                      height="50px"
+                    />
+                  </Link>
+                </div>
               <Avatar icon="user" />
+              </div>
             </Header>
             <Content style={{ padding: "0 50px", marginTop: 64 }}>
               <div
@@ -68,10 +102,7 @@ class CustomerLayout extends React.Component {
             <Footer style={{ textAlign: "center" }}>Created by Ryan</Footer>
           </Layout>
         ) : (
-          <Subscription
-            subscription={SUBSCRIBE_CART}
-            
-          >
+          <Subscription subscription={SUBSCRIBE_CART}>
             {({ data, loading }) => {
               if (!loading) {
                 console.log(data);
@@ -86,16 +117,61 @@ class CustomerLayout extends React.Component {
                   {({ loading, error, data }) => {
                     if (!loading) {
                       console.log(data);
-                      return <Layout>
-                          <Header style={{ position: "fixed", width: "100%", zIndex: 500, display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-                            <div style={{ display: "flex", flexDirection: "row", alignItems: "center", backgroundColor: '#fff', padding: '5px', margin: '5px' }}>
-                              <Link to="/"><Image src={HeaderLogo} alt="logo" height="50px" /></Link>
+                      return (
+                        <Layout>
+                          <Header
+                            style={{
+                              position: "fixed",
+                              width: "100%",
+                              zIndex: 500,
+                              display: "flex",
+                              flexDirection: "row",
+                              justifyContent: "space-between"
+                            }}
+                          >
+                            <div
+                              style={{
+                                display: "flex",
+                                flexDirection: "row",
+                                alignItems: "center",
+                                backgroundColor: "#fff",
+                                padding: "5px",
+                                margin: "5px"
+                              }}
+                            >
+                              <Link to="/">
+                                <Image
+                                  src={HeaderLogo}
+                                  alt="logo"
+                                  height="50px"
+                                />
+                              </Link>
                             </div>
-                            <div style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between", width: "100px" }}>
-                            <Popover placement="bottomRight" title="Your cart" content="Item" trigger="click">
-                              <Avatar icon="shopping-cart" />
-                            </Popover>
-                              <Avatar icon="user" />
+                            <div
+                              style={{
+                                display: "flex",
+                                flexDirection: "row",
+                                alignItems: "center",
+                                justifyContent: "space-between",
+                                width: "100px"
+                              }}
+                            >
+                              <Popover
+                                placement="bottomRight"
+                                title="Your cart"
+                                content="Item"
+                                trigger="click"
+                              >
+                                <Avatar icon="shopping-cart" />
+                              </Popover>
+                              <Popover
+                              placement="bottomRight"
+                              title="User menu"
+                              content={<a href="https://ur-shop-graphql-server.now.sh/logout"><button onClick={this.logOut}>Log out</button></a>}
+                              trigger="click"
+                              >
+                                <Avatar icon="user" />
+                              </Popover>
                             </div>
                           </Header>
                           <Content style={{ padding: "0 50px", marginTop: 64 }}>
@@ -112,7 +188,8 @@ class CustomerLayout extends React.Component {
                           <Footer style={{ textAlign: "center" }}>
                             Created by Ryan
                           </Footer>
-                        </Layout>;
+                        </Layout>
+                      );
                     }
 
                     return <Spin />;
