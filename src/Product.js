cc2bb9d5adb1d4fbe4df605d4c9ba62fbc99f8a0
Ryan Liu <shiang@Ryans-MacBook-Pro.local>
2018-05-22 20:20:59 +1000

    Use vendorId passed from Auth context in Product component instead of using params.id from react router.

--- src/Product.js
+++ src/Product.js
@@ -4,59 +4,74 @@ import { Query, Subscription } from "react-apollo";
 import { Button } from "semantic-ui-react";
 import { Card, Icon, Image } from "semantic-ui-react";
 import { Spin, BackTop, notification } from "antd";
-import { PRODUCT_CREATED, CHATROOM_CREATED, FETCH_VENDOR } from "./queries";
-import { TokenContext } from './components/Auth'
-
-
+import { PRODUCT_CREATED, FETCH_VENDOR } from "./queries";
+import { TokenContext } from "./components/Auth";
 
 class Product extends Component {
+  render() {
+    return (
+      <TokenContext.Consumer>
+        {({ vendorId }) => (
+          <Query
+            query={FETCH_VENDOR}
+            variables={{ _id: vendorId }}
+          >
+            {({ loading, error, data }) => {
+              if (!loading) {
+                //console.log("Product Data: ", data);
+                //console.log("Hits props: ", this.props.hit);
 
+                return (
+                  <Card.Group stackable itemsPerRow="4">
+                    <BackTop />
+                    {data.vendor.products.map(product => {
+                      const {
+                        name,
+                        price,
+                        description,
+                        category,
+                        _id,
+                        images
+                      } = product;
 
-    render() {
-    return <Subscription subscription={CHATROOM_CREATED}>
-        {({ data, loading }) => {
-          if (!loading) {
-            // notification.open({
-            //     message: data.createChatroom.name,
-            //     description: 'A New Chatroom has been created! Please refresh the screen.'
-            // })
-            console.log(data);
-          }
-          return <Query query={FETCH_VENDOR} variables={{ _id: this.props.match.params.id }}>
-              {({ loading, error, data }) => {
-                if (!loading) {
-                  //console.log("Product Data: ", data);
-                  //console.log("Hits props: ", this.props.hit);
+                      const imageUrl =
+                        images.length > 0
+                          ? images[0].pictureUrl
+                          : "http://fillmurray.com/200/300";
 
-                  return <Card.Group stackable itemsPerRow="4">
-                      <BackTop />
-                      {data.vendor.products.map(product => {
-                        const { name, price, description, category, _id, images } = product;
+                      return (
+                        <Card
+                          key={_id}
+                          fluid
+                          raised
+                          onClick={() => {
+                            this.props.history.push(
+                              `/manager/${
+                              vendorId
+                              }/products/${_id}/edit`
+                            );
+                          }}
+                        >
+                          <Image src={imageUrl} fluid />
+                          <Card.Content>
+                            <Card.Header>{name}</Card.Header>
+                            <Card.Meta>{category}</Card.Meta>
+                            <Card.Description>{description}</Card.Description>
+                          </Card.Content>
+                          <Card.Content extra>${price}</Card.Content>
+                        </Card>
+                      );
+                    })}
+                  </Card.Group>
+                );
+              }
 
-                        const imageUrl = images.length > 0 ? images[0].pictureUrl : "http://fillmurray.com/200/300";
-
-                        return <Card key={_id} fluid raised onClick={() => {
-                              this.props.history.push(`/manager/${this.props.match.params.id}/products/${_id}/edit`);
-                            }}>
-                            <Image src={imageUrl} fluid />
-                            <Card.Content>
-                              <Card.Header>{name}</Card.Header>
-                              <Card.Meta>{category}</Card.Meta>
-                              <Card.Description>
-                                {description}
-                              </Card.Description>
-                            </Card.Content>
-                            <Card.Content extra>${price}</Card.Content>
-                          </Card>;
-                      })}
-                    </Card.Group>;
-                }
-
-                return <Spin />;
-              }}
-            </Query>;
-        }}
-      </Subscription>;
+              return <Spin />;
+            }}
+          </Query>
+        )}
+      </TokenContext.Consumer>
+    );
   }
 }
 
