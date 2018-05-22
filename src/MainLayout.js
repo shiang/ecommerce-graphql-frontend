22daaa4806cc028c8f6cdf69ba7532182157e860
Ryan Liu <shiang@Ryans-MacBook-Pro.local>
2018-05-22 20:57:58 +1000

    fixed header display issue.

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Layout, Menu, Icon, Input, Avatar, Badge, Popover } from "antd";
 import CreateProductPage from "./pages/CreateProductPage";
 import { TokenContext } from "./components/Auth";
+import HeaderLogo from "./image/dot-shop-logo.png";
 // import UpdateProductPage from "./pages/UpdateProductPage";
 const { Header, Content, Footer, Sider } = Layout;
 const Search = Input.Search;
@@ -18,66 +19,31 @@ export default class MainLayout extends React.Component {
     return (
     <TokenContext.Consumer>
       {({ signOut, vendorId }) => {
-        return (
-          <Layout>
-            <Sider
-              collapsible
-              collapsed={this.state.collapsed}
-              onCollapse={this.onCollapse}
-              style={{
-                minHeight: "100vh",
-                overflow: "auto",
-                position: "fixed",
-                left: 0
-              }}
-            >
+        return <Layout>
+            <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse} style={{ minHeight: "100vh", overflow: "auto", position: "fixed", left: 0 }}>
               <div className="logo">
-                <h2
-                  style={{
-                    textAlign: "center",
-                    marginTop: "10px",
-                    color: "white"
-                  }}
-                >
-                  Ur-Shop
-                </h2>
+                <Image src={HeaderLogo} alt="logo" height="50px" />
               </div>
               <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-                <SubMenu
-                  key="sub1"
-                  title={
-                    <span>
+                <SubMenu key="sub1" title={<span>
                       <Icon type="shop" style={{ fontSize: "20px" }} />
                       <span>Products</span>
-                    </span>
-                  }
-                >
-                  <Menu.Item
-                    key="1"
-                    onClick={() => {
+                    </span>}>
+                  <Menu.Item key="1" onClick={() => {
                       this.props.history.push(`/manager/${vendorId}/products`);
-                    }}
-                  >
+                    }}>
                     All Products
                   </Menu.Item>
-                  <Menu.Item
-                    key="2"
-                    onClick={() => {
+                  <Menu.Item key="2" onClick={() => {
                       this.props.history.push(`/manager/${vendorId}/products/new`);
-                    }}
-                  >
+                    }}>
                     Add Product
                   </Menu.Item>
                 </SubMenu>
-                <SubMenu
-                  key="sub2"
-                  title={
-                    <span>
+                <SubMenu key="sub2" title={<span>
                       <Icon type="shopping-cart" style={{ fontSize: "20px" }} />
                       <span>Orders</span>
-                    </span>
-                  }
-                >
+                    </span>}>
                   <Menu.Item key="3">New Orders</Menu.Item>
                   <Menu.Item key="4">Order History</Menu.Item>
                 </SubMenu>
@@ -85,43 +51,36 @@ export default class MainLayout extends React.Component {
                   <Icon type="area-chart" style={{ fontSize: "20px" }} />
                   <span>Dashboard</span>
                 </Menu.Item>
-                <Menu.Item
-                  key="6"
-                  onClick={() => {
+                <Menu.Item key="6" onClick={() => {
                     this.props.history.push(`/manager/${vendorId}/profile`);
-                  }}
-                >
+                  }}>
                   <Icon type="profile" style={{ fontSize: "20px" }} />
                   <span>Profile</span>
                 </Menu.Item>
               </Menu>
             </Sider>
             <Layout style={{ marginLeft: this.state.collapsed ? 100 : 200 }}>
-              <Header
-                style={{
-                  position: "fixed",
-                  width: "100%",
-                  zIndex: 500,
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "flex-end"
-                }}
-              >
-                <Popover
-                  placement="bottom"
-                  title="Menu"
-                  content={<button onClick={() => signOut()}>Sign out</button>}
-                  trigger="click"
-                >
+              <Header style={{ position: "fixed", width: "100%", zIndex: 500, display: "flex", flexDirection: "row", justifyContent: "flex-end" }}>
+               <div>
+                <Popover placement="bottom" title="Menu" content={<button
+                      onClick={() => signOut()}
+                    >
+                      Sign out
+                    </button>} trigger="click">
                   <Badge dot>
-                    <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                  <Avatar icon="user" />
                   </Badge>
                 </Popover>
+                </div>
               </Header>
 
               <Content style={{ padding: "0 50px", marginTop: 74 }}>
                 <div
-                  style={{ padding: 24, background: "#fff", minHeight: 360 }}
+                  style={{
+                    padding: 24,
+                    background: "#fff",
+                    minHeight: 360
+                  }}
                 >
                   {this.props.children}
                 </div>
@@ -130,8 +89,7 @@ export default class MainLayout extends React.Component {
                 Made with ❤️ by Ryan © 2018
               </Footer>
             </Layout>
-          </Layout>
-        );
+          </Layout>;
       }}
     </TokenContext.Consumer>
     )
